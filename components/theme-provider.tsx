2a49b9e1428602c484831bde144be2d8859a7d8d
manthan <manthan.jsharma@gmail.com>
2025-04-14 20:40:09 +0530

    unused vars

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,25 +2,12 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes";
 import { useEffect, useState } from "react";
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children }: ThemeProviderProps) {
   const [hasMounted, setHasMounted] = useState(false);
 
   useEffect(() => {
     setHasMounted(true);
   }, []);
-  //   return (
-  //  {hasMounted && (  <NextThemesProvider
-  //       attribute="class"
-  //       defaultTheme="dark"
-  //       enableSystem={false}
-  //       disableTransitionOnChange
-  //       {...props}
-  //     >
-  //       {children}
-  //         </NextThemesProvider>)
-  // }
-  //   )
-  // }
 
   return (
     <>
