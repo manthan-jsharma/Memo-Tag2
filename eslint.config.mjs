8bf56f85059b09176a844b03c107e2bc9e8c9d98
manthan <manthan.jsharma@gmail.com>
2025-04-14 20:40:22 +0530

    modified eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Add your custom rules here
+      "react/no-unescaped-entities": "off",
+      "no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
